869496c7efd50e8132049b1a04a7c255e6749d02
Student <example@example.com>
2019-01-23 11:17:03 -0800

    set up app using angular add first functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Menu } from './models/tap-room.model';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tap-room';
+  menus:  Menu [] = [
+    new Menu ("ginger","Dr.Kombucha", 4.99),
+    new Menu ("superBerry","Dr.Kombucha", 4.99),
+    new Menu ("trilogy","Synergy", 5.99),
+  ]
+
+  newDrink(name, brand, price) {
+    console.log(name, brand, price)
+    let newMenu = new Menu(name, brand, price);
+    this.menus.push(newMenu)
+  }
+
 }
---
+++ src/app/models/tap-room.model.ts
@@ -0,0 +1,3 @@
+export class Menu {
+  constructor(public name: string, public brand: string, public price: number) { }
+}
